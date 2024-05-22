231d2c743a542ef7feb9a145484dbb57075eb7f8
MansiAndhariya <mansiandharia13@gmail.com>
2024-05-22 20:51:34 +0530

    mansi

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// var dbCollation = require("./utils/db");
-// var ContactMailRouter = require("./routes/ContactMail");
+var dbCollation = require("./utils/db");
+var ContactMailRouter = require("./routes/ContactMail");
 
-app.use(cors()); // Add this line to enable CORS for all routes
-app.use(express.json());
+// app.use(cors()); // Add this line to enable CORS for all routes
+// app.use(express.json());
 app.use("/api/contact_mail", ContactMailRouter);
 
 app.get("/", (req, res) => res.send("Connection Success!"));
